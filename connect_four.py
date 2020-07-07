e483d92f5f7cb95cd75205e42d3e14009a6d0516
Jeffery Jones <Jeffery Jones>
2020-07-07 14:34:52 -0700

    defined methods to initialize, reset and print the board

--- connect_four.py
+++ connect_four.py
@@ -1,7 +1,22 @@
-import os
-
 grid = {}
 hasWon = False
 circle = '\u2B24'
 square = '\u2B1B'
-turn = 'black'
+turn = circle
+
+def resetBoard(char):
+    for y in range(1, 11):
+        for x in range(1,11):
+            grid[(x, y)] = char
+
+def printBoard():
+    for row in range(1,11):
+        rowStr = ''
+        for col in range(1,11):
+            rowStr += grid[row,col] + ' '
+        print(rowStr)
+
+resetBoard('x')
+grid[1,1] = circle
+grid[2,2] = square
+printBoard()
